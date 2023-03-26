2d2c8a83dcd68fd965e621d49c794a62b8a98cdd
tanagaRa <tatka.kordiak@gmail.com>
2023-03-26 21:35:33 +0300

    finished and checked the 4th example

---
+++ examples/example4_gr14_KordiakTatiana.c
@@ -0,0 +1,50 @@
+// Selection from table
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+	int i;
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	sqlite3 *db;
+	char *ErrMsg = 0;
+	int rc;
+	char *sql;
+
+	// Opening DB
+	rc = sqlite3_open("test.db", &db);
+
+	if (rc) {
+		fprintf("Error while opening db: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return 1;	
+	} else {
+		fprintf(stdout,"Database opened successfully\n");
+	}
+
+	// Creating SQL statement
+	sql = "SELECT * FROM Cars"; 
+	
+	//Executong SQL statement
+	rc = sqlite3_exec(db, sql, callback, 0, &ErrMsg);
+
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", ErrMsg);
+		sqlite3_free(ErrMsg);
+		sqlite3_close(db);
+		return 1;
+	} else {
+		fprintf(stdout,"Operation finished successfully.\n");
+	}
+
+	sqlite3_close(db);
+	return 0;
+}
